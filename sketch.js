b71cd6548a4c2807dcc9ced226730586f68e1f60
passduck <67808602+passduck@users.noreply.github.com>
2020-07-07 01:27:11 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var ALERT_TITLE = "facebook";
+var ALERT_TITLE = "Oops!";
 var ALERT_BUTTON_TEXT = "Ok";
 
 if(document.getElementById) {
@@ -58,9 +58,11 @@ function setup() {
   // console.log(banner.width);
   //   wdth = width/10;
   //   hght = height/10;
+  // push();
+  // rectMode(CENTER);
   username = createInput();
-  username.size(300, 30);
-  username.position(610, 250);
+  username.size(300, 32);
+  // username.position(width/2-150, 325);
   username.style('border', '1px solid black');
   username.style('border-color', 'rgb(236, 236, 236)');
   username.style('fontSize', '15px');
@@ -71,8 +73,8 @@ function setup() {
 
 
   password = createInput('', 'password');
-  password.size(300, 30);
-  password.position(610, 300);
+  password.size(300, 32);
+  // password.position(width/2-150, 375);
   password.style('border', '1px solid black');
   password.style('border-color', 'rgb(236, 236, 236)');
   password.style('fontSize', '15px');
@@ -81,25 +83,48 @@ function setup() {
   password.elt.style.paddingLeft = '10px'
   password.elt.style.outline = 'none'
   submit = createButton('<b>Log In</b>');
-  submit.size(300, 40);
-  submit.position(610, 350);
-  submit.style('background-color', 'rgb(66,103,178)');
+  submit.size(314, 40);
+  // submit.position(width/2-150, 425);
+  submit.style('background-color', '#4267B2');
   submit.style('border', '0px solid black');
   submit.style('color', 'white');
   submit.style('display', 'inline-block');
-  submit.style('font-size', '15px');
+  submit.style('font-size', '14px');
+  // submit.elt.style.paddingLeft = '10px';
   submit.mousePressed(login);
   newAccount = createButton('Create New Account');
   newAccount.size(200, 40);
-  newAccount.position(660, 470);
-  newAccount.style('background-color', 'rgb(68, 180, 44)');
+  // newAccount.position(width/2-90, 550);
+  newAccount.style('background-color', '#42B72A');
   newAccount.style('border', '0px solid black');
   newAccount.style('color', 'white');
   newAccount.style('display', 'inline-block');
-  newAccount.style('font-size', '15px');
+  newAccount.style('font-size', '14px');
   newAccount.mousePressed(newacc);
-
+// pop();
   // canvas.resize(banne);
+  newAccount2 = createButton('<b>CreateNewAccount</b>');
+  newAccount2.size(134, 30);
+  // newAccount.position(width/2-90, 550);
+  newAccount2.style('background-color', '#42B72A');
+  newAccount2.style('border', '0px solid black');
+  newAccount2.style('color', 'white');
+  newAccount2.style('display', 'inline-block');
+  newAccount2.style('font-size', '12px');
+  newAccount2.mousePressed(newacc);
+  newAccount2.style("font-family","Helvetica");
+// newAccount2.elt.style.font ='Helvetica';
+username.position(width/2-150, 250);
+      password.position(username.x, username.y+50);
+      submit.position(username.x, password.y+50);
+      newAccount.position(username.x+60, submit.y+125);
+      if(width/2-315>180){
+      newAccount2.position(width/2-315, 31.6);
+}else{
+        newAccount2.position(180, 31.6);
+}
+  
+
 }
 
 
@@ -141,46 +166,91 @@ function draw() {
   background(236, 236, 236);
 
   push();
+  rectMode(CENTER);
   fill(255);
   noStroke();
-  rect(0, 620, width, height);
-  image(banner, 0, 0, width, 83);
+  // rect(0, 620, width, height);
+  // image(banner, 0, 0, width, 83);
   pop();
   push();
+  rectMode(CENTER);
   fill(255);
   stroke(220);
   strokeWeight(0.1);
-  rect(455, 165, 610, 375);
+  rect(width/2, 350, 612, 375);
 
   push();
   // scale(5, 0, 0);
-  image(bottom, 0, 620, width, bottom.height / 1.3);
+  if(width/2-500>0){
+  translate(width/2-500, 0);
+  }
+  fill('#385898');
+  textSize(12);
+textFont('Helvetica');
+  text('English (US) Español  Français (France)  中文(简体)  العربية  Português (Brasil)  한국어  Italiano  Deutsch  हिन्दी  日本語 \n\nSign Up    Log In    Messenger    Facebook Lite    Watch    People    Pages    Page Categories    Places    Games    Locations    Market place    Facebook    Pay    Groups\nOculus   Portal   Instagram   Local   Fundraisers   Services   About   Create Ad   Create Page   Developers   Careers   Privacy   Cookies   Ad Choices   Terms\nHelp\n\nFacebook © 2020', 0, 740)
+  // image(bottom, 0, 620, bottom.width/1.2, bottom.height / 1.3);
+  fill(0);
+  // rectMode(LEFT, CORNER);
+  rect(590, 750, width/1.3, 0.3);
   pop();
   pop();
   push();
   textSize(19);
-  text('Log Into Facebook', 675, 220);
+  text('Log Into Facebook', width/2-60, 220);
   pop();
 
   push();
   fill(66, 103, 150);
   textSize(12);
-  text('Forgot account?', 720, 415);
+  text('Forgot account?', password.x+115, submit.y+75);
   pop();
 
   push();
   noStroke();
   fill(0);
-  rect(610, 440, 130, 0.3);
-  rect(780, 440, 130, 0.3);
+  rect(password.x, submit.y+110, 130, 0.3);
+  rect(password.x+180, submit.y+110, 130, 0.3);
   textSize(11);
-  text('or', 755, 445);
+  text('or', password.x+150, submit.y+110);
   pop();
 
-
-
+  push();
+  textSize(37);
+  textStyle(BOLD);
+  fill(100, 100, 200);
+  rectMode(CENTER);
+  noStroke();
+  image(banner, 0, 0, width*10, 83);
+  // rect(width/2, 40, width, 80);
+  fill(255);
+  // if(width>1000){
+  if(width/2-500>0){
+  translate(width/2-500, 0);
+  }
+    textFont('Tahoma');
+    text('facebook', 0, 60);
+// }else{
+  // text('facebook', 0, 60);
+// }
+    // text('facebook', width/5.6, 60);
+// }
+  pop();
 
 
   // image(bckgrnd, 0, 0);
   // text('hi', width/2, height/2);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, height);
+  if(width/2-150>150){
+      username.position(width/2-150, 250);
+      password.position(username.x, username.y+50);
+      submit.position(username.x, password.y+50);
+      newAccount.position(username.x+60, submit.y+125);
+  }
+  if(width/2-315>180){
+      newAccount2.position(width/2-315, 31.6);
+}else{
+        newAccount2.position(180, 31.6);
+}
 }
